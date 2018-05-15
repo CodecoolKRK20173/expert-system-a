fa4d79f5266685cf70422f24fe2e5a2f8dc7b388
polon59 <przemyslaw.polczak@gmail.com>
2018-05-15 22:08:57 +0200

    Add list of answers in class field and initialise it in constructor

--- Answer.java
+++ Answer.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Answer {
-    
+    private List<Value> possibleValues;
+
+    public Answer(){
+        this.possibleValues = new ArrayList<Value>();
+    }
 
     public boolean evaluateAnswerByInput(String input){
-
+        return true;
     }
 
     public void addValue(Value value){
-
+        this.possibleValues.add(value);
     }
 }
