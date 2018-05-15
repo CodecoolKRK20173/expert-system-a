2eb55007eff47b43e51ccda61bda0e5126c8db9d
polon59 <przemyslaw.polczak@gmail.com>
2018-05-15 21:33:14 +0200

    Implement simple main method for testing class functionality

--- RuleParser.java
+++ RuleParser.java
@@ -3,10 +3,12 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 public class RuleParser extends XMLParser{
+    private RuleRepository ruleRepository;
 
     public static void main(String[] args) {
         RuleParser ruleParser = new RuleParser();
         ruleParser.getRuleRepository();
+       
     }
 
     public RuleParser(){
@@ -14,20 +16,28 @@ public class RuleParser extends XMLParser{
     }
 
     public RuleRepository getRuleRepository(){
-        RuleRepository ruleRepository = new RuleRepository();
-        
+        ruleRepository = new RuleRepository();
         NodeList nList = document.getElementsByTagName("Rule");
 
         for (int temp = 0; temp < nList.getLength(); temp++) {
             Node nNode = nList.item(temp);
-            Element eElement = (Element) nNode;
+            Element element = (Element) nNode;
             
-            String question = eElement.getElementsByTagName("Question")
-            .item(0).getTextContent();
- 
+            Question question = createQuestion(element);
+            ruleRepository.addQuestion(question);
         }
-        return ruleRepository;
-            
+        ruleRepository.printAllQuestions();
+        return ruleRepository;      
     }
+
+    private Question createQuestion(Element element){
+        String id = element.getAttribute("id");
+        String questionText = element.getElementsByTagName("Question").item(0).getTextContent();
+        Answer answer = new Answer(); // need to add other code here (read answer from XML)
+        Question question = new Question(id, questionText, answer);
+        return question;
+    }
+
+
     
 }
