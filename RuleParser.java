8e07eb4e9b2aed88ac9ff77cfa469594150af668
polon59 <przemyslaw.polczak@gmail.com>
2018-05-15 22:30:05 +0200

    Test functionality

--- RuleParser.java
+++ RuleParser.java
@@ -23,19 +23,34 @@ public class RuleParser extends XMLParser{
             Node nNode = nList.item(temp);
             Element element = (Element) nNode;
             
-            Question question = createQuestion(element);
+            Question question = readQuestion(element);
             ruleRepository.addQuestion(question);
         }
         ruleRepository.printAllQuestions();
         return ruleRepository;      
     }
 
-    private Question createQuestion(Element element){
+    private Question readQuestion(Element element){
         String id = element.getAttribute("id");
         String questionText = element.getElementsByTagName("Question").item(0).getTextContent();
-        Answer answer = new Answer(); // need to add other code here (read answer from XML)
+        //Answer answer = new Answer(); // need to add other code here (read answer from XML)
+
+        NodeList nodeA = element.getElementsByTagName("Answer").item(0);
+        Answer answer = readAnswer(nodeA);
+
         Question question = new Question(id, questionText, answer);
         return question;
+    }
+
+    private Answer readAnswer(NodeList nodeA) {
+        Element answerElement = (Element)nodeA;
+        NodeList answersList = answerElement.getElementsByTagName("Selection");
+        Answer createdAnswer = new Answer();
+
+        for (int i = 0; i < answersList.getLength(); i++) {
+            createdAnswer.addValue(getValueObject(answersList.item(i)));
+        }
+        return createdAnswer;
     }
 
 
