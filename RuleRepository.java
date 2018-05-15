ab495ff48cec02ca7db419927b0a19cf471cc7f4
polon59 <przemyslaw.polczak@gmail.com>
2018-05-15 11:13:10 +0200

    Add class 'RuleRepository' with it's methods

---
+++ RuleRepository.java
@@ -0,0 +1,13 @@
+import java.util.Iterator;
+
+public class RuleRepository {
+
+    public void addQuestion(Question question){
+
+    }
+
+    public Iterator<Question> getIterator(){
+        
+    }
+    
+}
