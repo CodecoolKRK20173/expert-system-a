187214f03cce2e17cc6a054f926667176ca8f041
polon59 <przemyslaw.polczak@gmail.com>
2018-05-15 11:10:49 +0200

    Add class 'Fact' with it's methods

---
+++ Fact.java
@@ -0,0 +1,24 @@
+import java.util.Set;
+
+public class Fact {
+
+    public Fact(String id, String description){
+
+    }
+
+    public Set<String> getIdSet(){
+
+    }
+
+    public void setFactValueById(String id, boolean value){
+
+    }
+
+    public boolean getValueById(String id, boolean value){
+
+    }
+
+    public String getDescription(){
+        
+    }
+}
