86927076a4bda2ec1f96449f1a5cae045c395275
polon59 <przemyslaw.polczak@gmail.com>
2018-05-15 11:09:29 +0200

    Add class 'ESProvider' with it's methods

---
+++ ESProvider.java
@@ -0,0 +1,18 @@
+public class ESProvider{
+
+    public ESProvider(FactParser factParser, RuleParser ruleParser){
+
+    }
+    
+    public void collectAnswer(){
+
+    }
+
+    public boolean getAnswerByQuestion(String questionID){
+
+    }
+
+    public String evaluate(){
+        
+    }
+}
