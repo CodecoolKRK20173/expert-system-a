6737865a743da7de9d353b51b16818375043236c
polon59 <przemyslaw.polczak@gmail.com>
2018-05-15 21:01:02 +0200

    Redefine constructor body to use constructor from the parent class 'XMLParser'

--- RuleParser.java
+++ RuleParser.java
@@ -1,12 +1,33 @@
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 public class RuleParser extends XMLParser{
 
-    public RuleParser(){
-        loadXmlDocument("Rules.xml");
+    public static void main(String[] args) {
+        RuleParser ruleParser = new RuleParser();
+        ruleParser.getRuleRepository();
     }
-    public RuleRepository getRuleRepository(){
-        
 
+    public RuleParser(){
+        super("Rules.xml");
+    }
+
+    public RuleRepository getRuleRepository(){
+        RuleRepository ruleRepository = new RuleRepository();
+        
+        NodeList nList = document.getElementsByTagName("Rule");
+
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+            Node nNode = nList.item(temp);
+            Element eElement = (Element) nNode;
+            
+            String question = eElement.getElementsByTagName("Question")
+            .item(0).getTextContent();
+ 
+        }
+        return ruleRepository;
+            
     }
     
 }
