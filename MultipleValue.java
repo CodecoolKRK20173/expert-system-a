4387973b5becb6e5cb1d8617cee5131a13895d1c
Paweł Stój <routi1992@gmail.com>
2018-05-16 16:39:14 +0200

    Add two parameters

--- MultipleValue.java
+++ MultipleValue.java
@@ -1,8 +1,8 @@
 import java.util.List;
 
-public class MultipleValue extends value {
+public class MultipleValue extends Value {
 
     public MultipleValue(List<String> params, boolean selectionType){
-        
+        super(params, selectionType);
     }
 }
