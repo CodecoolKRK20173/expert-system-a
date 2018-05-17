0ed02f8f3704c0d67c897c256789ec85f7582030
Polikkox <damian.targosz11@gmail.com>
2018-05-17 13:49:08 +0200

    Add evaluate method

--- ESProvider.java
+++ ESProvider.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
@@ -12,8 +13,16 @@ public class ESProvider{
         this.factParser = factParser;
         this.ruleParser = ruleParser;
     }
+    public void runApplication(){
+        collectAnswer();
+        display(evaluate());
+    }
+    public void display(String message){
+        System.out.println("Perfet firearm for you is: " + message);
+    }
     
     public void collectAnswer(){
+
         Iterator<Question> questionIterator = ruleParser.getRuleRepository().getIterator();
         questionAnswers = new HashMap<String, Boolean>();
         Question question;
@@ -24,22 +33,49 @@ public class ESProvider{
             input = getUserInput();
             questionAnswers.put(question.getId(), question.getEvaluatedAnswer(input));
         }
-        
-
-
     }
 
-    public boolean getAnswerByQuestion(String questionID){
-        return true;
+    private boolean getAnswerByQuestion(String questionID){
+        return questionAnswers.get(questionID);
     }
+    private boolean ifFactChoosen(Fact fact){
+        for(String evalId : fact.getIdSet()){
+            if(fact.getValueById(evalId, fact.getValues().get(evalId)) == this.getAnswerByQuestion(evalId)){
+                return true;
+            }
+        }
+        return false;
+    }   
 
     public String evaluate(){
-        return "Test";
+        Iterator<Fact> factIterator = factParser.getFactRepository().getIterator();
+        Fact fact;
+
+        while(factIterator.hasNext()){
+            fact = factIterator.next();
+            if(ifFactChoosen(fact)){
+                return fact.getDescription();
+            }
+        }
+        return null;
     }
+
+
+
+
+
+
     public String getUserInput(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your answer ");
-        String input = scanner.nextLine();
-        return input;
+      
+        try{
+            String input = scanner.nextLine();
+            return input;
+        }
+        catch(InputMismatchException e){
+            System.out.println("Something wrong is with format ");
+        }
+        return null;
     }
 }
