68f1d09ecd81764a1d2e73ea694680c6b434becd
Polikkox <damian.targosz11@gmail.com>
2018-05-17 12:03:39 +0200

    Add collect answer method

--- ESProvider.java
+++ ESProvider.java
@@ -1,6 +1,12 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
 public class ESProvider{
     private FactParser factParser;
     private RuleParser ruleParser;
+    private Map<String, Boolean> questionAnswers;
 
     public ESProvider(FactParser factParser, RuleParser ruleParser){
         this.factParser = factParser;
@@ -8,7 +14,18 @@ public class ESProvider{
     }
     
     public void collectAnswer(){
+        Iterator<Question> questionIterator = ruleParser.getRuleRepository().getIterator();
+        questionAnswers = new HashMap<String, Boolean>();
+        Question question;
+        String input;
         
+        while(questionIterator.hasNext()){
+            question = questionIterator.next();
+            input = getUserInput();
+            questionAnswers.put(question.getId(), question.getEvaluatedAnswer(input));
+        }
+        
+
 
     }
 
@@ -18,5 +35,11 @@ public class ESProvider{
 
     public String evaluate(){
         return "Test";
+    }
+    public String getUserInput(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your answer ");
+        String input = scanner.nextLine();
+        return input;
     }
 }
