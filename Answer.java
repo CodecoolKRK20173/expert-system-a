62ea93e7f2fb3d566fe9f512acc8d3d87060adb8
Polikkox <damian.targosz11@gmail.com>
2018-05-17 22:17:40 +0200

    Refactoring and clean code

--- Answer.java
+++ Answer.java
@@ -7,32 +7,36 @@ public class Answer {
     private List<Value> possibleValues;
 
     public Answer(){
-
         this.possibleValues = new ArrayList<Value>();
     }
 
     public boolean evaluateAnswerByInput(String input) throws InputMismatchException {
 
-        boolean reponse = false;
+        boolean respone = false;
         if (input.equals("yes") || input.equals("no")) {
             for (Value value : possibleValues) {
-                for (String option : value.getInputPattern()) {
-                    if (option.equals(input)){
-                        return reponse = true;
-                    } else {
-                        return reponse = false;
-                    }
-                }
+                return respone = checkCorretoption(value, input);
             }
         } 
         else{
             throw new InputMismatchException("Answer must have only [yes] or [no] answer ");
         } 
-        return reponse;
+        return respone;
+    }
+
+    private boolean checkCorretoption(Value value, String input){
+
+        for (String option : value.getInputPattern()) {
+            if (option.equals(input)){
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
     }
 
     public void addValue(Value value){
-        
         this.possibleValues.add(value);
     }
 
