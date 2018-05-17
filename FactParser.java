172bbcfdb4e287ea769be9e9d1b65f7daf4f24b5
Polikkox <damian.targosz11@gmail.com>
2018-05-17 11:14:45 +0200

    Add correct fact parser

--- FactParser.java
+++ FactParser.java
@@ -5,13 +5,45 @@ import java.util.*;
 
 public class FactParser extends XMLParser{
 
-    public FactParser() {
-        
+    private FactRepository factRepo;
+
+    public FactParser(){
         super("Facts.xml");
-        loadXmlDocument("Facts.xml");
+        factRepo = new FactRepository();
+    }
+
+    public void parseFacts(){        
+        NodeList nList = document.getElementsByTagName("Fact");
+
+        for (int i = 0; i < nList.getLength(); i++) {
+
+            Node nNode = nList.item(i);
+
+            Element eElement = (Element) nNode;
+            String id = eElement.getAttribute("id");
+            String description = eElement.getChildNodes().item(1).getAttributes().item(0).getNodeValue();
+            Fact fact = new Fact(id, description);
+            int evalsCount = eElement.getElementsByTagName("Eval").getLength();
+
+            for(int k = 0; k < evalsCount; k++){
+                boolean evalBoolean = Boolean.valueOf(eElement.getElementsByTagName("Eval").item(k).getTextContent());    
+                String evalID = eElement.getElementsByTagName("Eval").item(k).getAttributes().item(0).getNodeValue();
+                // if(eElement.getElementsByTagName("Eval").item(k).getTextContent().equals("true")){
+                //     evalBoolean = true;
+                // }
+                // else{
+                //     evalBoolean = false;
+                // }
+               
+                fact.addIdValue(evalID, evalBoolean);
+            }
+            
+            factRepo.addFact(fact);
+        }
+        System.out.println(factRepo.getFacts().size());
     }
 
     public FactRepository getFactRepository(){
-         return new FactRepository();         
+         return this.factRepo;
     }
 }
